b855b6bcd7de8e16e2d970c3210efff8b9329ff5
zac <yaowanagbeyond2010@163.com>
2025-10-27 23:16:49 +0800

    ✨ feat: 添加博客logo,更改导航栏为中文显示

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  website: "https://blog.xcoral.top/", // replace this with your deployed domain
   author: "zac",
-  profile: "https://satnaing.dev/", //个人站点
-  desc: "我的博客.", // 网站描述
-  title: "AstroPaper", //  网站名字
-  ogImage: "astropaper-og.jpg", // 分享时的图
+  profile: "https://blog.xcoral.top/", //个人站点
+  desc: "记录一些生活的日常.", // 网站描述
+  title: "随记", //  网站名字
+  ogImage: "blog-og.jpg", // 分享时的图
   lightAndDarkMode: true, // 支持亮色和暗色模式切换
   postPerIndex: 4, // 首页每页文章数量
   postPerPage: 4, // 文章列表页每页文章数量
@@ -14,7 +14,7 @@ export const SITE = {
   editPost: {
     enabled: true,
     text: "Edit page",
-    url: "https://github.com/satnaing/astro-paper/edit/main/",
+    url: "https://github.com/zacattle/zblog/issues",
   }, // 是否允许用户对文章提建议
   dynamicOgImage: true, // 是否启用动态OG图片生成,文章很多时建议通过trade-off关闭
   dir: "ltr", // "rtl" | "auto" // 网站文字方向 rtl为从右到左 auto为根据语言自动判断
--- src/constants.ts
+++ src/constants.ts
@@ -2,12 +2,12 @@ import type { Props } from "astro";
 import IconMail from "@/assets/icons/IconMail.svg";
 import IconGitHub from "@/assets/icons/IconGitHub.svg";
 import IconBrandX from "@/assets/icons/IconBrandX.svg";
-import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
+// import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
 import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
 import IconFacebook from "@/assets/icons/IconFacebook.svg";
 import IconTelegram from "@/assets/icons/IconTelegram.svg";
 import IconPinterest from "@/assets/icons/IconPinterest.svg";
-import { SITE } from "@/config";
+// import { SITE } from "@/config";
 
 interface Social {
   name: string;
@@ -19,28 +19,28 @@ interface Social {
 export const SOCIALS: Social[] = [
   {
     name: "GitHub",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on GitHub`,
+    href: "https://github.com/zacattle",
+    linkTitle: `GitHub`,
     icon: IconGitHub,
   },
-  {
-    name: "X",
-    href: "https://x.com/username",
-    linkTitle: `${SITE.title} on X`,
-    icon: IconBrandX,
-  },
-  {
-    name: "LinkedIn",
-    href: "https://www.linkedin.com/in/username/",
-    linkTitle: `${SITE.title} on LinkedIn`,
-    icon: IconLinkedin,
-  },
-  {
-    name: "Mail",
-    href: "mailto:yourmail@gmail.com",
-    linkTitle: `Send an email to ${SITE.title}`,
-    icon: IconMail,
-  },
+  // {
+  //   name: "X",
+  //   href: "https://x.com/username",
+  //   linkTitle: `${SITE.title} on X`,
+  //   icon: IconBrandX,
+  // },
+  // {
+  //   name: "LinkedIn",
+  //   href: "https://www.linkedin.com/in/username/",
+  //   linkTitle: `${SITE.title} on LinkedIn`,
+  //   icon: IconLinkedin,
+  // },
+  // {
+  //   name: "Mail",
+  //   href: "mailto:yourmail@gmail.com",
+  //   linkTitle: `Send an email to ${SITE.title}`,
+  //   icon: IconMail,
+  // },
 ] as const;
 
 export const SHARE_LINKS: Social[] = [
